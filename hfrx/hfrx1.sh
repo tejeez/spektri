775b110db88767e7007807d2d628afd66667718a
Tatu Peltola <tatu.peltola@gmail.com>
2021-04-30 13:24:56 +0300

    Add HF spectrum analysis script

---
+++ hfrx/hfrx1.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+# HF spectrum analyzer for RX888
+
+FFTSIZE=16384
+SAMPLERATE=80000000
+
+# libsddc repository should be cloned into ../../libsddc
+# and built into ../../libsddc/build
+LIBSDDC="../../libsddc"
+DATA="../data"
+mkdir -p "${DATA}"
+
+# On a hyperthreading machine, it seems to be more efficient
+# to use only one logical core from each physical core
+# for the heavy computation.
+# This is only used for testing on a i5-3360M laptop.
+# On non-hyperthreading machines, these should be removed.
+TASKSET="taskset -c 0,2"
+# Run the other processes on the other logical cores.
+TASKSET2="taskset -c 1,3"
+#TASKSET2="taskset -c 1"
+#TASKSET2=""
+
+# Put sddc_stream in loop, so that it is restarted if it fails
+(while true; do
+ ${TASKSET2} "${LIBSDDC}/build/src/sddc_stream" "${LIBSDDC}/firmware/SDDC_FX3.img" "${SAMPLERATE}"
+done) \
+| ${TASKSET2} pv \
+| ${TASKSET} ../spektri/target/release/spektri real "${FFTSIZE}" \
+> "${DATA}/hf_$(date +%Y%m%d_%H%M%S)_${SAMPLERATE}_${FFTSIZE}.data"
