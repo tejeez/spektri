f4d662f88e7be141f321d95b2753b67afa160113
Tatu Peltola <tatu.peltola@gmail.com>
2021-05-24 21:13:07 +0300

    Add more input formats

--- spektri/src/main.rs
+++ spektri/src/main.rs
@@ -11,34 +11,12 @@ mod dsp;
 mod multifft;
 
 
-arg_enum! {
-    #[derive(Debug, Copy, Clone)]
-    enum InputFormat {
-        S16le,   // real signed 16-bit, little endian
-        Cs16le,  // complex signed 16-bit, little endian
-    }
-}
-
-fn bytes_per_input_sample(fmt: InputFormat) -> usize {
-    match fmt {
-        InputFormat::S16le  => 2,
-        InputFormat::Cs16le => 4,
-    }
-}
-
-fn is_input_format_complex(fmt: InputFormat) -> bool {
-    match fmt {
-        InputFormat::S16le  => false,
-        InputFormat::Cs16le => true,        
-    }
-}
-
-
 fn parse_configuration() -> (dsp::DspParams, InputFormat) {
     use clap::{App};
     let matches = App::new("spektri")
         .args_from_usage("
             -s, --fftsize=[SIZE]             'FFT size'
+            -b, --fftbuf=[NUMBER]            'Number of FFTs in each input buffer (adjust to optimize performance)'
             -a, --averages=[NUMBER]          'Number of FFTs averaged for spectrum'
             -f, --inputformat=[FORMAT]       'Input signal format'
             -F, --spectrumformat=[FORMAT]    'Spectrum output format'
@@ -46,14 +24,27 @@ fn parse_configuration() -> (dsp::DspParams, InputFormat) {
         .get_matches();
 
     let inputformat = value_t!(matches, "inputformat", InputFormat).unwrap_or_else(|e| e.exit());
+
     (dsp::DspParams {
-        complex: is_input_format_complex(inputformat),
-        fft_size: value_t!(matches, "fftsize", usize).unwrap_or(16384),
-        scaling: 1.0 / std::i16::MAX as f32,
-        ffts_per_buf: 8,
-        spectrum_format: if matches.value_of("spectrumformat").unwrap_or("u8") == "u16" { dsp::SpectrumFormat::U16 } else { dsp::SpectrumFormat::U8 },
-        spectrum_averages: value_t!(matches, "averages", u32).unwrap_or(2000),
-    }, inputformat)
+        complex:
+            is_input_format_complex(inputformat),
+        fft_size:
+            value_t!(matches, "fftsize", usize)
+            .unwrap_or(16384),
+        scaling:
+            input_format_scaling(inputformat),
+        ffts_per_buf:
+            value_t!(matches, "fftbuf", usize)
+            .unwrap_or(8),
+        spectrum_format:
+            value_t!(matches, "inputformat", SpectrumFormat)
+            .unwrap_or(SpectrumFormat::U8)
+            .into(),
+        spectrum_averages:
+            value_t!(matches, "averages", u32)
+            .unwrap_or(2000),
+    },
+    inputformat)
 }
 
 
@@ -122,13 +113,115 @@ fn mainloop_real(dspparams: dsp::DspParams, fmt: InputFormat) -> std::io::Result
 }
 
 
+/*
+ * Input formats and conversion functions
+ */
+
+
+arg_enum! {
+    #[derive(Debug, Copy, Clone)]
+    enum InputFormat {
+        U8,      // real unsigned 8-bit
+        S8,      // real signed 8-bit
+        S16le,   // real signed 16-bit, little endian
+        S16be,   // real signed 16-bit, big endian
+        F32le,   // real float 32-bit, little endian
+        F32be,   // real float 32-bit, big endian
+        Cu8,     // complex unsigned 8-bit
+        Cs8,     // complex signed 8-bit
+        Cs16le,  // complex signed 16-bit, little endian
+        Cs16be,  // complex signed 16-bit, big endian
+        Cf32le,  // complex float 32-bit, little endian
+        Cf32be,  // complex float 32-bit, big endian
+    }
+}
+
+fn bytes_per_input_sample(fmt: InputFormat) -> usize {
+    match fmt {
+        InputFormat::U8     => 1,
+        InputFormat::S8     => 1,
+        InputFormat::S16le  => 2,
+        InputFormat::S16be  => 2,
+        InputFormat::F32le  => 4,
+        InputFormat::F32be  => 4,
+        InputFormat::Cu8    => 2,
+        InputFormat::Cs8    => 2,
+        InputFormat::Cs16le => 4,
+        InputFormat::Cs16be => 4,
+        InputFormat::Cf32le => 8,
+        InputFormat::Cf32be => 8,
+    }
+}
+
+fn is_input_format_complex(fmt: InputFormat) -> bool {
+    match fmt {
+        InputFormat::U8     |
+        InputFormat::S8     |
+        InputFormat::S16le  |
+        InputFormat::S16be  |
+        InputFormat::F32le  |
+        InputFormat::F32be  => false,
+        InputFormat::Cu8    |
+        InputFormat::Cs8    |
+        InputFormat::Cs16le |
+        InputFormat::Cs16be |
+        InputFormat::Cf32le |
+        InputFormat::Cf32be => true,
+    }
+}
+
+/* Scaling factors for each input format to get numbers between -1 and 1 */
+fn input_format_scaling(fmt: InputFormat) -> f32 {
+    match fmt {
+        InputFormat::U8     |
+        InputFormat::S8     |
+        InputFormat::Cu8    |
+        InputFormat::Cs8    => 1.0 / std::i8::MAX as f32,
+        InputFormat::S16le  |
+        InputFormat::S16be  |
+        InputFormat::Cs16le |
+        InputFormat::Cs16be => 1.0 / std::i16::MAX as f32,
+        InputFormat::F32le  |
+        InputFormat::F32be  |
+        InputFormat::Cf32le |
+        InputFormat::Cf32be => 1.0,
+    }
+}
+
+
 fn convert_to_f32(src: &[u8], dst: &mut [f32], fmt: InputFormat) {
     let mut offset = 0;
     match fmt {
+        InputFormat::U8 =>
+        for v in dst.iter_mut() {
+            *v = src.read_with::<u8>(&mut offset, LE).unwrap() as f32 - 127.4;
+        },
+
+        InputFormat::S8 =>
+        for v in dst.iter_mut() {
+            *v = src.read_with::<i8>(&mut offset, LE).unwrap() as f32;
+        },
+
         InputFormat::S16le =>
-            for v in dst.iter_mut() {
-                *v = src.read_with::<i16>(&mut offset, LE).unwrap() as f32;
-            },
+        for v in dst.iter_mut() {
+            *v = src.read_with::<i16>(&mut offset, LE).unwrap() as f32;
+        },
+
+        InputFormat::S16be =>
+        for v in dst.iter_mut() {
+            *v = src.read_with::<i16>(&mut offset, BE).unwrap() as f32;
+        },
+
+        InputFormat::F32le =>
+        for v in dst.iter_mut() {
+            *v = src.read_with::<f32>(&mut offset, LE).unwrap();
+        },
+
+        InputFormat::F32be =>
+        for v in dst.iter_mut() {
+            *v = src.read_with::<f32>(&mut offset, BE).unwrap();
+        },
+
         _ => panic!("real conversion called with complex format parameter") // bug somewhere
     }
 }
@@ -136,13 +229,75 @@ fn convert_to_f32(src: &[u8], dst: &mut [f32], fmt: InputFormat) {
 fn convert_to_cf32(src: &[u8], dst: &mut [Complex<f32>], fmt: InputFormat) {
     let mut offset = 0;
     match fmt {
+        InputFormat::Cu8 =>
+        for v in dst.iter_mut() {
+            *v = Complex{
+                re: src.read_with::<u8>(&mut offset, LE).unwrap() as f32 - 127.4,
+                im: src.read_with::<u8>(&mut offset, LE).unwrap() as f32 - 127.4
+            }
+        },
+
+        InputFormat::Cs8 =>
+        for v in dst.iter_mut() {
+            *v = Complex{
+                re: src.read_with::<i8>(&mut offset, LE).unwrap() as f32,
+                im: src.read_with::<i8>(&mut offset, LE).unwrap() as f32
+            }
+        },
+
         InputFormat::Cs16le =>
-            for v in dst.iter_mut() {
-                *v = Complex{
-                    re: src.read_with::<i16>(&mut offset, LE).unwrap() as f32,
-                    im: src.read_with::<i16>(&mut offset, LE).unwrap() as f32
-                }
-            },
+        for v in dst.iter_mut() {
+            *v = Complex{
+                re: src.read_with::<i16>(&mut offset, LE).unwrap() as f32,
+                im: src.read_with::<i16>(&mut offset, LE).unwrap() as f32
+            }
+        },
+
+        InputFormat::Cs16be =>
+        for v in dst.iter_mut() {
+            *v = Complex{
+                re: src.read_with::<i16>(&mut offset, BE).unwrap() as f32,
+                im: src.read_with::<i16>(&mut offset, BE).unwrap() as f32
+            }
+        },
+
+        InputFormat::Cf32le =>
+        for v in dst.iter_mut() {
+            *v = Complex{
+                re: src.read_with::<f32>(&mut offset, LE).unwrap(),
+                im: src.read_with::<f32>(&mut offset, LE).unwrap()
+            }
+        },
+
+        InputFormat::Cf32be =>
+        for v in dst.iter_mut() {
+            *v = Complex{
+                re: src.read_with::<f32>(&mut offset, BE).unwrap(),
+                im: src.read_with::<f32>(&mut offset, BE).unwrap()
+            }
+        },
+
         _ => panic!("complex conversion called with real format parameter") // bug somewhere
+    }
+}
+
+
+/* Define a separate SpectrumFormat enum here
+ * to avoid depending on clap in the dsp module. */
+
+arg_enum! {
+    #[derive(Debug, Copy, Clone)]
+    enum SpectrumFormat {
+        U8,
+        U16
+    }
+}
+
+impl From<SpectrumFormat> for dsp::SpectrumFormat {
+    fn from(fmt: SpectrumFormat) -> Self {
+        match fmt {
+            SpectrumFormat::U8  => dsp::SpectrumFormat::U8,
+            SpectrumFormat::U16 => dsp::SpectrumFormat::U16,
+        }
     }
 }
