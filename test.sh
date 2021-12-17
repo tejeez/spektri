0020dd934cd4b1f4f98f3cd82351192f4477b310
Tatu Peltola <tatu.peltola@gmail.com>
2021-12-17 23:50:59 +0200

    Add filters in test script

--- test.sh
+++ test.sh
@@ -5,10 +5,12 @@ mkdir -p data/
 (cd testsignal; cargo build --release)
 (cd spektri; cargo build --release)
 # Test the spectrum analyzer with a frequency sweep
-time testsignal/target/release/testsignal complex 100000000 | (time spektri/target/release/spektri --inputformat=cs16le --fftsize=1024 --averages=100) > data/testspectrum.data
-time testsignal/target/release/testsignal real 100000000 | (time spektri/target/release/spektri --inputformat=s16le --fftsize=1024 --averages=100) > data/testspectrum_real.data
+P="--fftsize=1024 --averages=100 --filters data/filtered"
+time testsignal/target/release/testsignal complex 100000000 | (time spektri/target/release/spektri --inputformat=cs16le $P) > data/testspectrum.data
+P+=_real
+time testsignal/target/release/testsignal real 100000000 | (time spektri/target/release/spektri --inputformat=s16le $P) > data/testspectrum_real.data
 # Test another output format
-time testsignal/target/release/testsignal real 100000000 | (time spektri/target/release/spektri --inputformat=s16le --fftsize=1024 --averages=100 --spectrumformat=u16) > data/testspectrum_real16.data
+time testsignal/target/release/testsignal real 100000000 | (time spektri/target/release/spektri --inputformat=s16le --spectrumformat=u16 $P) > data/testspectrum_real16.data
 # Display the resulting spectrogram by interpreting the output as a raw image file
 display -size 1024x1302 -depth 8 GRAY:data/testspectrum.data &
 display -size 513x1302 -depth 8 GRAY:data/testspectrum_real.data &
